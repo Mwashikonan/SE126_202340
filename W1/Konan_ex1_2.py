0cd3eea0bf0331067408978ceb6e5143b569ad7d
Tanya <takonan@email.neit.edu>
2023-08-14 22:25:47 -0400

    WEEK 1 WORK

--- W1/Konan_ex1_2.py
+++ W1/Konan_ex1_2.py
@@ -1,27 +1,52 @@
 import time
 
-fin = "y"
-
 for x in range (6):
     print (x)
 
-for x in range (10):
+def valX() :
     
-    print ("***Value of x = " + str(x) +"***" + "\r\n")
-    print ("Repetition #:" + str(x) + "\r\n")
-    print ("1. Render Graphics" + "\r\n")
-    print ("2. Render Audio" + "\r\n")
-    print ("3. Execute AI" + "\r\n")
-    print ("4. Perform Networking" + "\r\n")
-    print ("5. Strobe Keyboard/mouse" + "\r\n")
-    
-    time.sleep(2)
-    
-    fin = input("Would you like to continue? [Y/N]")
-    
-    while fin != "Y" and fin != "y":
+    for x in range (10):
         
-        print ("Thank You for playing\r\n")
+        print ("***Value of x = " + str(x) +"***" + "\r\n")
+        print ("Repetition #:" + str(x) + "\r\n")
+        print ("1. Render Graphics" + "\r\n")
+        print ("2. Render Audio" + "\r\n")
+        print ("3. Execute AI" + "\r\n")
+        print ("4. Perform Networking" + "\r\n")
+        print ("5. Strobe Keyboard/mouse" + "\r\n")
+        
+        time.sleep(2)
+    
+valX()
+    
+while True :
+    
+    choice = input("Would you like to contiue? [Y/N]")
+    
+    if str(choice) == "Y" or str(choice) == "y" :
+        valX()
+        
+    if str(choice) == "N" or str(choice) == "n" :
+        
         print("----END OF GAME LOOP----")
+        
+        break
+        
+    #Error trap
+    while str(choice) != "Y" and str(choice) != "y" and str(choice) != "n" and str(choice) != "N" :
+         
+         print("INVALID ENTRY!")
+         
+         choice = input("Would you like to contiue? [Y/N]")
+         
+         if str(choice) == "Y" or str(choice) == "y" :
+            valX()
+        
+         if str(choice) == "N" or str(choice) == "n" :
+        
+            print("----END OF GAME LOOP----")
+        
+            break
+         
         
         
--- W1/Konan_ex1_3.py
+++ W1/Konan_ex1_3.py
@@ -1,52 +1,12 @@
 #Function def
 
-def DefaultArg(name, msg = "Hello!"):
-    print (name)
-    print (msg)
-
-    return #no value returned
-
-#Function called
-
-DefaultArg("Tanya") # No second argument
-
-DefaultArg("Tanya", "Yo!") # With second argument
-
-
-
-
-
-
-
-#Function called
-
-#DefaultArg2("Tanya") # No second argument
-
-#DefaultArg2("Tanya", "Yo!") # With second argument
-
-def DefaultArg2(name, msg = "Hello!"):
-    
-    print (name)
-    print (msg)
-
-    return #no value returned
-
-
-
-
-
-
-
-
-
-#Function def
-
-def DefaultArg3 (name, msg = "Hello!", num = 7):
+def DefaultArg3 (name, msg = "Hello!", num= 7):
     
     print (name)
     print (msg)
     print (num)
-    return 7 #value returned
+    
+    return num #value returned
 
 #Function called
 
